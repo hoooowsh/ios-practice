36a1118da8d1a88463c9dad80bc5aed7873e8ebe
hoooowsh <83986522+hoooowsh@users.noreply.github.com>
2023-05-18 11:24:09 -0400

    first init

---
+++ im rich/im rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  im rich
+//
+//  Created by Sheng Hao on 5/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
